efef541b91e7bdab82fd4c68dc492a1a13cd7e27
Brandon Abbott <brandon.abbott@charter.net>
2019-07-07 12:22:18 -0700

    forgot to add screens

---
+++ lib/roleintroductionscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hitlerrolechooserflutter/hitlerrolechooser.dart';
+import 'package:hitlerrolechooserflutter/main.dart';
+import 'package:provider/provider.dart';
+
+class RoleIntroductionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Player Introduction"),
+      ),
+      body: Center(child: Text("Placeholder"))
+    );
+  }
+}
---
+++ lib/roundscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hitlerrolechooserflutter/hitlerrolechooser.dart';
+import 'package:hitlerrolechooserflutter/main.dart';
+import 'package:provider/provider.dart';
+
+class RoundScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Main Menu"),
+      ),
+      body: Center(child: Text("Placeholder"))
+    );
+  }
+}
