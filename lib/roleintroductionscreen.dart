96ee6acd5a760f9e42bb92781922f3dc61e0411e
Brandon Abbott <brandon.abbott@charter.net>
2019-07-07 16:15:18 -0700

    dartfmt

--- lib/roleintroductionscreen.dart
+++ lib/roleintroductionscreen.dart
@@ -7,29 +7,38 @@ class RoleIntroductionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Player Introduction"),
-      ),
-      body: GridView.builder(
-        itemCount:
-            Provider.of<GameState>(context).game.PlayerList.length,
-        gridDelegate:
-        SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(child: Text(Provider.of<GameState>(context).game.PlayerList[index].name, style: TextStyle(fontSize: 32), textAlign: TextAlign.center, overflow: TextOverflow.fade,), onPressed: (){print("i'm pressed");}),
-          );
-        }
-      )
-    );
+        appBar: AppBar(
+          title: Text("Player Introduction"),
+        ),
+        body: GridView.builder(
+            itemCount: Provider.of<GameState>(context).game.PlayerList.length,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RaisedButton(
+                    child: Text(
+                      Provider.of<GameState>(context)
+                          .game
+                          .PlayerList[index]
+                          .name,
+                      style: TextStyle(fontSize: 32),
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.fade,
+                    ),
+                    onPressed: () {
+                      print("i'm pressed");
+                    }),
+              );
+            }));
   }
 }
 
 class RevealedList with ChangeNotifier {
   List<Player> revealedList;
-void addPlayer(Player player){
-  //add it
-  notifyListeners();
-}
+  void addPlayer(Player player) {
+    //add it
+    notifyListeners();
+  }
 }
