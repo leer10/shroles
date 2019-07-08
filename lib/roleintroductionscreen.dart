c897b83c44462c76b3ee1bf4efcf9a1e10ccc8e3
Brandon Abbott <brandon.abbott@charter.net>
2019-07-07 18:46:52 -0700

    attempt to have "opened roles" through a different provider but doesn't really owrk

--- lib/roleintroductionscreen.dart
+++ lib/roleintroductionscreen.dart
@@ -10,35 +10,52 @@ class RoleIntroductionScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Player Introduction"),
         ),
-        body: GridView.builder(
-            itemCount: Provider.of<GameState>(context).game.PlayerList.length,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: RaisedButton(
-                    child: Text(
-                      Provider.of<GameState>(context)
-                          .game
-                          .PlayerList[index]
-                          .name,
-                      style: TextStyle(fontSize: 32),
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.fade,
-                    ),
-                    onPressed: () {
-                      print("i'm pressed");
-                    }),
-              );
-            }));
+        body: ChangeNotifierProvider(
+          builder: (context) => RevealedList(),
+          child: GridView.builder(
+              itemCount: Provider.of<GameState>(context).game.PlayerList.length,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: RaisedButton(
+                      child: Text(
+                        Provider.of<GameState>(context)
+                            .game
+                            .PlayerList[index]
+                            .name,
+                        style: TextStyle(fontSize: 32),
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.fade,
+                      ),
+                      onPressed: (!Provider.of<RevealedList>(context)
+                              .revealedList
+                              .contains(Provider.of<GameState>(context)
+                                  .game
+                                  .PlayerList[index]))
+                          ? () {
+                            print("added");
+                              Provider.of<RevealedList>(context)
+                                  .addPlayer(Provider.of<GameState>(context)
+                                      .game
+                                      .PlayerList[index]);
+                            }
+                          : null),
+                );
+              }),
+        ));
   }
 }
 
 class RevealedList with ChangeNotifier {
-  List<Player> revealedList;
+  List<Player> revealedList = [];
   void addPlayer(Player player) {
     revealedList.add(player);
+  // AALKJ:DSJFKS HOW TO REFERENCE OTHER SSTATE  
+  //  if (revealedList.length == Provider.of<GameState>(context)
+  //      .game
+  //      .PlayerList.length)
     notifyListeners();
   }
 }
