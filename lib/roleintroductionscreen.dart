f5bc6747f6409cc11a3c1553c9c1aa12e91ed499
Brandon Abbott <brandon.abbott@charter.net>
2019-07-10 15:42:39 -0700

    dartfmt and fix for web

--- lib/roleintroductionscreen.dart
+++ lib/roleintroductionscreen.dart
@@ -10,25 +10,28 @@ class RoleIntroductionScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text("Player Introduction"),
         ),
-        body: OrientationBuilder(
-          builder: (context, orientation) {
-            return GridView.builder(
-                itemCount: Provider.of<GameState>(context).game.PlayerList.length,
-                gridDelegate:
-                    (orientation == Orientation.portrait) ? SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2) : SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: _PlayerButton(player: Provider.of<GameState>(context).game.PlayerList[index]),
-                  );
-                });
-          }
-        ));
+        body: OrientationBuilder(builder: (context, orientation) {
+          return GridView.builder(
+              itemCount: Provider.of<GameState>(context).game.PlayerList.length,
+              gridDelegate: (orientation == Orientation.portrait)
+                  ? SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2)
+                  : SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _PlayerButton(
+                      player: Provider.of<GameState>(context)
+                          .game
+                          .PlayerList[index]),
+                );
+              });
+        }));
   }
 }
 
 class _PlayerButton extends StatefulWidget {
-  const _PlayerButton({ Key key, this.player }) : super(key: key);
+  const _PlayerButton({Key key, this.player}) : super(key: key);
   final Player player;
 
   @override
@@ -42,14 +45,12 @@ class _PlayerButtonState extends State<_PlayerButton> {
     isAvailable = widget.player.spoiled;
     return RaisedButton(
         child: Text(
-          widget.player
-              .name,
+          widget.player.name,
           style: TextStyle(fontSize: 32),
           textAlign: TextAlign.center,
           overflow: TextOverflow.fade,
         ),
-        onPressed: (!widget.player
-                .spoiled)
+        onPressed: (!widget.player.spoiled)
             ? () async {
                 print("added");
 
@@ -59,28 +60,21 @@ class _PlayerButtonState extends State<_PlayerButton> {
                       barrierDismissible: false,
                       builder: (BuildContext context) {
                         return AlertDialog(
-                            title: Text(
-                                "${widget.player.name}"),
+                            title: Text("${widget.player.name}"),
                             content: SingleChildScrollView(
                                 child: ListBody(children: <Widget>[
-                              if (widget.player
-                                      .role ==
-                                  Role.fascist)
+                              if (widget.player.role == Role.fascist)
                                 Text("You're a Fascist."),
-                              if (widget.player
-                                      .role ==
-                                  Role.hitler)
+                              if (widget.player.role == Role.hitler)
                                 Text("You're Hitler."),
-                              if (widget.player
-                                  .isLiberal)
+                              if (widget.player.isLiberal)
                                 Text("You're a Liberal."),
                               if (Provider.of<GameState>(context)
                                       .game
                                       .viewOthers(widget.player) !=
                                   null)
                                 for (Player player
-                                    in Provider.of<GameState>(
-                                            context)
+                                    in Provider.of<GameState>(context)
                                         .game
                                         .viewOthers(widget.player))
                                   Text(
@@ -97,14 +91,22 @@ class _PlayerButtonState extends State<_PlayerButton> {
                 }
 
                 await _presentRole();
-                setState((){Provider.of<GameState>(context, listen:false).introducePlayer(
-                    widget.player);
-                  isAvailable = false;});
+                setState(() {
+                  Provider.of<GameState>(context, listen: false)
+                      .introducePlayer(widget.player);
+                  isAvailable = false;
+                });
                 if (Provider.of<GameState>(context).introduced == true) {
-                  Scaffold.of(context).showSnackBar(SnackBar(content: Text("Everyone's in!"), backgroundColor: Colors.green,action: SnackBarAction(
-                    label: "Back to the Menu", textColor: Colors.white,
-                    onPressed: () {Navigator.popUntil(context, ModalRoute.withName("/roundscreen"));}
-                  )));
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("Everyone's in!"),
+                      backgroundColor: Colors.green,
+                      action: SnackBarAction(
+                          label: "Back to the Menu", //textColor: Colors.white,
+                          onPressed: () {
+                            Navigator.popUntil(
+                                context, ModalRoute.withName("/roundscreen"));
+                          })
+                        ));
                 }
               }
             : null);
